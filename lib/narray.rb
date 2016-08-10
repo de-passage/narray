20890ded450190573fd6ef5a53a6f7f502124732
depassage <maisbiensurqueoui@gmail.com>
2016-08-10 09:58:39 +0900

    Complete the constructor and add #at

--- lib/narray.rb
+++ lib/narray.rb
@@ -2,23 +2,35 @@
 
 class NArray < Array
 	attr_reader :dimensions
-	def initialize dimensions, values = nil
+	def initialize dimensions, values = nil, &blck
 		if dimensions.is_a? Array and dimensions.each { |d| NArray.valid_dimension? d }
 			@dimensions = dimensions.length
 			if @dimensions > 1 
-				super(dimensions[0]).map! { |e| NArray.new(dimensions.slice(1..-1), values) }
+				super(dimensions[0]).map! { |e| NArray.new(dimensions.slice(1..-1), values, &blck) }
 			else
-				super(dimensions[0], values)
+				if block_given?
+					super(dimensions[0], &blck)
+				else
+					super(dimensions[0], values)
+				end
 			end
 		elsif NArray.valid_dimension? dimensions
 			@dimensions = dimensions
+			if @dimensions > 1
+				super(1) { NArray.new(dimensions - 1, values, &blck) }
+			else 
+				if block_given?
+					super(0, &blck)
+				else
+					super(0, values)
+				end
+			end
 		else
-			raise "Invalid dimensions"
+			raise "Invalid dimensions (must be a strictly positive integer or an array of lengths)"
 		end
 	end
 
 	def [] position, *positions
-		raise "#{positions.length + 1} positions instead of #{dimensions}" unless positions.length + 1 == dimensions
 		positions.empty? ? super(position) : super(position)[*positions]
 	end
 
@@ -29,6 +41,11 @@ class NArray < Array
 		else
 			_at(position)[*positions] = value
 		end
+	end
+
+	def at(position, *positions)
+		raise "#{positions.length + 1} positions instead of #{dimensions}" unless positions.length + 1 == dimensions
+		
 	end
 
 	def length d = 0
@@ -47,11 +64,16 @@ class NArray < Array
 
 end
 
+n = NArray.new([3, 2, 4]) { [*(1..100)].sample }
+n2 = NArray.new(2, 42)
+puts "#{n2[0, 0] = 42}"
+puts "#{n2}"
 
-n = NArray.new([3, 2, 4], 42)
 n[0, 1, 3] = "a"
 n[0,0,0] = 0
 puts n.inspect
 for i in 0...n.dimensions
 	puts "dimension #{i}.length == #{n.length(i)}"
 end
+
+n.each do |e| puts "Hola #{e}!" end
