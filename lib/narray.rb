a0524a5577b3a7bfd0884b379712d67d0f057f76
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2016-08-12 10:33:48 +0900

    Add .gitignore, constructor possibilities, attribute accessors, tests

--- lib/narray.rb
+++ lib/narray.rb
@@ -1,14 +1,57 @@
 # Multidimensional array for Ruby
 
 class NArray < Array
-	def initialize dimensions = nil, *values, &blck
-		if dimensions.is_a? NArray
-			self.replace dimensions
+	attr_reader :dimensions
+	def initialize dimensions = nil, values = nil, &blck
+		# In case the only parameter is a NArray, duplicate it
+		if dimensions.is_a? NArray and values == nil
+			self.replace dimensions.dup
+		# In case the parameter is an array, multiple possibilities
 		elsif dimensions.is_a? Array
-			NArray.is_well_formed? dimensions
-			@dimensions = calculate_dimensions dimensions
-			super dimensions
+			# 1) The array decribes dimensions and provide a default value to fill in the blanks
+			if NArray.is_valid_description? dimensions and (!values.nil? or block_given?)
+				# then we build the n-array recursively and fill the values with what has been provided
+				@dimensions = dimensions.length
+				@dimensions == 1 ?
+					super(dimensions[0], values, &blck) :
+					super([NArray.new(dimensions.drop(1), values, &blck)])
+			# 2) the array does not provide a default value
+			elsif values.nil? and !block_given?
+				# then we create a NArray fitting the litteral given
+				@dimensions = NArray.count_dimensions dimensions #inefficient but GTD
+				@dimensions == 1 ?
+					super(dimensions) : # giving a block here has no effect with mri and the doc doesn't say anything
+					super(dimensions.map { |e| NArray.new(e) })
+			# 3) the array is not a valid description but a default value is given, i.e. user mistake. Scold him!
+			else
+				raise RuntimeError "#{dimensions} is not a valid description: 
+				An array of striclty positive Integers is expected"
+			end
+		elsif NArray.is_valid_dimension? dimensions
+			@dimensions = dimensions
+			if dimensions == 1
+				super([], &blck)
+			else 
+				super([NArray.new(dimensions - 1, values, &blck)])
+			end
+		elsif NArray.is_valid_description?
+
+		elsif dimensions.nil? and values.nil?
+			super(&blck)
+		else
+			raise RuntimeError \
+				"Invalid dimension (expecting an Integer or array of Integer all strictly positives, got #{dimensions}"
 		end
+	end
+
+	# Returns the number of dimensions of the n-array
+	def dimensions
+		NArray.count_dimensions(self)
+	end
+
+	# Returns an array of the lengths of each dimension
+	def lengths
+		NArray.calculate_dimensions(self)
 	end
 
 	class << self
@@ -22,6 +65,20 @@ class NArray < Array
 		# Return an array of lengths for a n-array
 		def calculate_dimensions array
 			_count(array).take_while { |e| e >= 0 }
+		end
+
+		# Check whether the argument is a valid dimension
+		#
+		# Returns true if dimensions is a strictly positive Integer, false otherwise
+		def is_valid_dimension? dimensions
+			dimensions.is_a? Integer and dimensions > 0
+		end
+
+		# Check whether the argument is a valid description of dimensions
+		#
+		# Returns true if the argument is an Array of values satisfying is_valid_dimension?, false otherwise
+		def is_valid_description? dimensions
+			dimensions.is_a? Array and dimensions.all? { |e| is_valid_dimension? e }
 		end
 
 		private
--- test/narray_test.rb
+++ test/narray_test.rb
@@ -10,13 +10,52 @@ class TestNArray < MiniTest::Test
 #	def test_a_narray_can_be_created_from_a_template
 #		assert_equal NArray.new( [[1,2,3], [1,2,3], [1,2,3]] ), [[1,2,3], [1,2,3], [1,2,3]]
 #	end
-
-	def test_class_calculate_dimensions_correctly
-		assert_equal NArray::calculate_dimensions(Array.new(3) { Array.new (3) }), [3,3]
-		assert_equal NArray::calculate_dimensions([]), [0]
-		assert_equal NArray::calculate_dimensions([[], 43, [1, 2], nil]), [4]
-		assert_equal NArray::calculate_dimensions("test"), []
+	
+	def setup
+		@d3 = NArray.new([[[1,"a"], [[], 5]], [["test", []], [nil, [1,2]]]])
+	end
+	
+	def test_a_narray_can_be_created_from_a_dimension
+		assert_equal [], NArray.new(1), "Unidimensional Array: "
+		assert_equal [[]], NArray.new(2), "Bidimensional Array: "
+		assert_equal [[[]]], NArray.new(3), "Tridimensional Array: "
+		assert_equal 69, NArray::count_dimensions(NArray.new(69)), "69-dimensional Array (no kidding!): "
 	end
 
+	def test_a_narray_can_be_created_from_a_litteral_array
+		assert_equal [1,2,3], NArray.new([1,2,3])
+		assert_equal [], NArray.new([])
+		assert_equal [[1,2], [1,2]], NArray.new([[1,2], [1,2]])
+		assert_equal [[[1,"a"], [[], 5]], [["test", []], [nil, [1,2]]]],
+			NArray.new([[[1,"a"], [[], 5]], [["test", []], [nil, [1,2]]]])
+	end
+
+	def test_a_narray_should_always_have_a_dimensions_attribute_set
+		assert_equal 1, NArray.new(1).dimensions
+		assert_equal 2, NArray.new(2).dimensions
+		assert_equal 42, NArray.new(42).dimensions
+	end
+
+	def test_class_calculate_dimensions_correctly
+		assert_equal [3,3], NArray::calculate_dimensions(Array.new(3) { Array.new (3) })
+		assert_equal [0], NArray::calculate_dimensions([])
+		assert_equal [4], NArray::calculate_dimensions([[], 43, [1, 2], nil])
+		assert_equal [], NArray::calculate_dimensions("test")
+		assert_equal [4, 3, 2], NArray::calculate_dimensions(
+			Array.new(4) { 
+				Array.new(3) { 
+					Array.new(2) { [*(1..10)].sample }}})
+	end
+
+	def test_class_count_dimensions_correctly
+		assert_equal 2, NArray::count_dimensions(Array.new(3) { Array.new(3) })
+		assert_equal 1, NArray::count_dimensions([])
+		assert_equal 1, NArray::count_dimensions([[], 43, [1, 2], nil])
+		assert_equal 0, NArray::count_dimensions("test")
+		assert_equal 3, NArray::count_dimensions(
+			Array.new(4) { 
+				Array.new(3) { 
+					Array.new(2) { [*(1..10)].sample }}})
+	end
 end
 
